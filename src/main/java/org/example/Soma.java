b4baa270967425ed5314aacf3de4cb021f8171b6
Mike Andrade <mike.andrade.oliveira@gmail.com>
2023-03-02 19:02:14 -0300

    algoritmo de soma

---
+++ src/main/java/org/example/Soma.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Soma {
+    public static void main(String[] args) {
+        int indice = 13;
+        int soma = 0;
+        int k = 0;
+
+        while(k < indice){
+            k += 1;
+            soma += k;
+        }
+
+        System.out.println(soma);
+    }
+}
